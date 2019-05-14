d068ca9180048ef1a76e898b6aa8c82dcc5b7b3e
SzGoral <szczepan.goral@gmail.com>
2019-05-14 22:07:29 +0200

    dodano widok i szablon szczegolowego postu, dodano CSS dla strony

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name='post_list_ale_sprawdzam_gdzie_to_sie_wyswietla'),
     path('data', views.current_datetime, name='cotojest?'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 import datetime
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
@@ -14,3 +14,8 @@ def current_datetime(request):
     now = datetime.datetime.now()
     html = "<html><body>It is now %s.</body></html>" % now
     return HttpResponse(html)
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
